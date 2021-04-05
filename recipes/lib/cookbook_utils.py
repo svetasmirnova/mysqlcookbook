03767735408cd1514917123cfe881980d20d1e44
svetasmirnova <sveta@js-client.com>
2021-04-05 03:05:20 +0300

    Python examples for chapter 14

--- recipes/lib/cookbook_utils.py
+++ recipes/lib/cookbook_utils.py
@@ -101,6 +101,8 @@ def get_enumorset_info(conn, db_name, tbl_name, col_name):
   return info
 #@ _GET_ENUMORSET_INFO_
 
+# Check whether a value is a legal ENUM value.
+
 #@ _CHECK_ENUM_VALUE_
 def check_enum_value(conn, db_name, tbl_name, col_name, val):
   valid = 0
@@ -116,3 +118,157 @@ def check_enum_value(conn, db_name, tbl_name, col_name, val):
         break
   return valid
 #@ _CHECK_ENUM_VALUE_
+
+# Check whether a value is a legal SET value; each element must be
+# a legal element of the column.  (For example, 'blue,white' is a legal
+# value for a set defined as 'red,white,blue'.)
+
+# An empty string is always a legal set element.  No special test
+# is required, because for the empty value, split returns an empty
+# list and the loop never executes.
+
+#@ _CHECK_SET_VALUE_
+def check_set_value(conn, db_name, tbl_name, col_name, val):
+  pass
+#@ _CHECK_SET_VALUE_
+
+
+# Trim whitespace from the beginning and end of a string, returning
+# the result.
+
+#@ _TRIM_WHITESPACE_
+def trim_whitespace(val):
+  res = re.sub('^\s+', '', val)
+  res = re.sub('\s+$', '', res)
+  return res
+#@ _TRIM_WHITESPACE_
+
+
+# Return true if argument represents an integer (possible leading sign),
+# an unsigned (nonnegative) integer, or a positive integer.
+
+#@ _IS_INTEGER_
+def is_integer(val):
+  p = re.compile('^[-+]?\d+$')
+  return p.match(val)
+#@ _IS_INTEGER_
+
+#@ _IS_UNSIGNED_INTEGER_
+def is_unsigned_integer(val):
+  p = re.compile('^\d+$')
+  return p.match(val)
+#@ _IS_UNSIGNED_INTEGER_
+
+#@ _IS_POSITIVE_INTEGER_
+def is_positive_integer(val):
+  p = re.compile('^\+?\d+$')
+  s = p.search(val)
+  return s and (s.group(0) != '0')
+#@ _IS_POSITIVE_INTEGER_
+
+
+# Return true if argument represents a valid innings-pitched number
+# (.1, 1, 1.2, etc.  Fractional digit, if present, must be 0, 1, or 2.)
+
+#@ _IS_INNINGS_PITCHED_
+def is_innings_pitched(val):
+  pass
+#@ _IS_INNINGS_PITCHED_
+
+
+# Functions to check whether a string looks like a date in various
+# common formats.  All permit the year to be 2 or 4 digits.  All permit
+# delimiters to be any nondigit character.
+
+# Each returns undef if date doesn't match the pattern, otherwise returns
+# a reference to an array containing the year, month, and day parts.
+# Does not perform bounds testing on the parts.
+
+#@ _IS_ISO_DATE_
+def is_iso_date(val):
+  m = re.match('^(\d{2,4})\D(\d{1,2})\D(\d{1,2})$', val)
+  return [m.group(1),  m.group(2), m.group(3)] if m else None
+#@ _IS_ISO_DATE_
+
+#@ _IS_MMDDYY_DATE_
+def is_mmddyy_date(val):
+  pass
+#@ _IS_MMDDYY_DATE_
+
+#@ _IS_DDMMYY_DATE_
+def is_ddmmyy_date(val):
+  pass
+#@ _IS_DDMMYY_DATE_
+
+# Functions to check whether a string looks like a time in 24-hour
+# format, or 12-hour format with an optional AM/PM suffix.  (Note that a
+# 12-hour time like 12:00:00 PM becomes a 24-hour time like 12:00:00, not
+# 24:00:00. And 12:xx:xx AM is 00:xx:xx in 24-hour time)
+# The hour can be 1 or 2 digits.  The minute and second must be two digits.
+# Delimiters can any nondigit character.
+
+#@ _IS_24HR_TIME_
+def is_24hr_time(val):
+  pass
+#@ _IS_24HR_TIME_
+
+#@ _IS_AMPM_TIME_
+def is_ampm_time(val):
+  m = re.match('^(\d{1,2})\D(\d{2})\D(\d{2})(?:\s*(AM|PM))?$', val, flags = re.I)
+  if m is None:
+    return None
+  (hour, min, sec) = (m.group(1), m.group(2), m.group(3))
+  # supply missing seconds
+  sec = '00' if sec is None else sec
+  if int(hour) == 12 and (m.group(4) is None or m.group(4).upper() == "AM"):
+    hour = '00' # 12:xx:xx AM times are 00:xx:xx
+  elif int(hour) < 12 and (m.group(4) is not None) and m.group(4).upper() == "PM":
+    hour = int(hour) + 12 # PM times other than 12:xx:xx
+  return [hour, min, sec] # return hour, minute, second
+#@ _IS_AMPM_TIME_
+
+# Determine whether a year is a leap year.  Requires 4-digit
+# year value (e.g., "1993", not "93").
+
+#@ _IS_LEAP_YEAR_
+def is_leap_year(year):
+  pass
+#@ _IS_LEAP_YEAR_
+
+
+# Given an array of integers representing year, month, and day of a date,
+# determine whether the date is valid.
+
+#@ _IS_VALID_DATE_
+def is_valid_date(year, month, day):
+  pass
+#@ _IS_VALID_DATE_
+
+
+# Given an array of integers representing hour, minute, and second,
+# determine whether the time is valid.
+
+#@ _IS_VALID_TIME_
+def is_valid_time(hour, minute, second):
+  pass
+#@ _IS_VALID_TIME_
+
+
+# Convert 2-digit year to 4 digits (return unchanged if not 2-digits).
+# The optional second argument indicates the transition point at which
+# values change from 2000-based to 1900-based.  The default is to switch
+# at 70, like MySQL.  (Thus 00..69 -> 2000..2069 and 70..99 -> 1970..1999)
+
+#@ _YY_TO_CCYY_
+def yy_to_ccyy(year, transition_point):
+  pass
+#@ _YY_TO_CCYY_
+
+
+# Given year and month, return the number of days in the month.  Year
+# value must be in 4-digit form or the leap-year test won't be correct.
+
+#@ _DAYS_IN_MONTH_
+def days_in_month(year, month):
+  pass
+#@ _DAYS_IN_MONTH_
