2d1961902a0e08402e0c4f50c4ac4e835ed1e545
svetasmirnova <sveta@js-client.com>
2021-04-15 22:16:59 +0300

    examples for chapter 14: new files

---
+++ recipes/transfer/yy_to_ccyy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# yy_to_ccyy.py: Test function yy_to_ccyy
+
+import cookbook
+from cookbook_utils import *
+
+print(yy_to_ccyy(60))
+print(yy_to_ccyy(1960))
+print(yy_to_ccyy(60, 50))
+print(yy_to_ccyy(1960, 50))
+print(yy_to_ccyy(40, 50))
+print(yy_to_ccyy(1940, 50))
+
