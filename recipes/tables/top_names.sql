078135bc766bf76abf9ef9e3057064cf7ea82177
svetasmirnova <sveta@js-client.com>
2021-05-23 23:15:10 +0300

    top_names.sql correction and new tables

--- recipes/tables/top_names.sql
+++ recipes/tables/top_names.sql
@@ -6,7 +6,6 @@ DROP TABLE IF EXISTS top_names;
 #@ _CREATE_TABLE_
 
 CREATE TABLE top_names (
-names_id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
 top_name VARCHAR(25),
 name_rank SMALLINT,
 name_count INT,
@@ -17,8 +16,8 @@ pctblack DECIMAL(2,2),
 pctapi DECIMAL(2,2),
 pctaian DECIMAL(2,2),
 pct2prace DECIMAL(2,2),
-pcthispanic DECIMAL(2,2),
-PRIMARY KEY (names_id)) 
+pcthispanic DECIMAL(2,2)
+)
 ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-LOAD DATA LOCAL INFILE 'Names_2010Census.csv' INTO TABLE top_names;
+LOAD DATA LOCAL INFILE '../datasets/Names_2010Census.csv' INTO TABLE top_names FIELDS TERMINATED BY ',' IGNORE 1 LINES;
