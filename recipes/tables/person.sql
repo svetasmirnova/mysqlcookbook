54c213a323d746e9b0f8e1ba0b887eae92993c20
svetasmirnova <sveta@js-client.com>
2021-07-26 03:52:31 +0300

    Table person modification

--- recipes/tables/person.sql
+++ recipes/tables/person.sql
@@ -2,10 +2,11 @@
 
 DROP TABLE IF EXISTS `person`;
 #@ _CREATE_TABLE_
+CREATE TABLE `person`(
+  `tax_id` int unsigned NOT NULL,
   `last_name` char(20) NOT NULL,
   `first_name` char(20) NOT NULL,
-  `address` char(40) DEFAULT NULL,
-  PRIMARY KEY (`last_name`,`first_name`)
+  `address` char(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 #@ _CREATE_TABLE_
 
