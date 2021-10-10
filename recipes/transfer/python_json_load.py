a784e1d101e523f0630d5d1b718ac4de01c484c8
Alkin Tezuysal <alkin.tezuysal@gmail.com>
2021-10-10 07:47:19 +0300

    Adding ch07 related scripts replacing kjv
    Signed-off-by: Alkin Tezuysal <alkin.tezuysal@gmail.com>

---
+++ recipes/transfer/python_json_load.py
@@ -0,0 +1,32 @@
+#!env python
+# Pre-requisites 
+# 1. $ sudo pip install mysql-connector-python
+# 2. $ curl -L -s http://deepyeti.ucsd.edu/jianmo/amazon/categoryFilesSmall/Appliances_5.json.gz | gunzip > /tmp/Appliances_5.json
+# 3. $ mysql cookbook < reviews.sql
+# 4. $ ./python_json_load.py /tmp/Appliances_5.json
+
+import mysql.connector
+import os
+import sys
+
+filepath = sys.argv[1]
+if not os.path.isfile(filepath):
+    print("File path {} does not exist. Exiting...".format(filepath))
+    sys.exit(1)
+
+db = mysql.connector.connect(host='localhost',
+                             database='cookbook',
+                             user='cbuser',
+                             password='cbpass',auth_plugin='mysql_native_password')
+
+cur = db.cursor(prepared=True)
+sql = "INSERT INTO reviews VALUES (0,%s)"
+
+with open(filepath) as file:
+    line = file.readline()
+    while line:
+        cur.execute(sql, (line,))
+        db.commit()
+        line = file.readline()
+
+sys.exit(0)
