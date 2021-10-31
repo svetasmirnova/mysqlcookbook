25b9093bbc88d7f9b8deda613d31bb88e6528769
Alkin Tezuysal <alkin.tezuysal@gmail.com>
2021-10-31 15:45:43 +0300

    Add new table for bit_count function example
    Signed-off-by: Alkin Tezuysal <alkin.tezuysal@gmail.com>

---
+++ recipes/tables/sundays.sql
@@ -0,0 +1,21 @@
+# sundays.sql 
+
+# Table to demonstrate finding number of Sundays in a month. 
+
+DROP TABLE `sundays`;
+CREATE TABLE `sundays` (
+    year YEAR(4), 
+    month INT(2) UNSIGNED ZEROFILL,
+    day INT(2) UNSIGNED ZEROFILL
+);
+
+# Load data by inserting all Sundays in November 2021
+
+INSERT INTO Sundays VALUES(2021,11,7),
+                            (2021,11,14),
+                            (2021,11,21),
+                            (2021,11,28);
+
+# Select the number of Sundays 
+SELECT year, month, BIT_COUNT(BIT_OR(1<<day)) AS days FROM Sundays
+       GROUP BY year,month;
