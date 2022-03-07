8cb370b9b91ef35f4654b774bac019e2b636ac67
Alkin Tezuysal <alkin.tezuysal@gmail.com>
2022-03-07 21:16:58 +0300

    8.14 corrections and updates GG review
    Signed-off-by: Alkin Tezuysal <alkin.tezuysal@gmail.com>

--- recipes/tables/sibling.sql
+++ recipes/tables/sibling.sql
@@ -14,3 +14,18 @@ INSERT INTO sibling (name,birth) VALUES('Ilayda','2002-12-17');
 INSERT INTO sibling (name,birth) VALUES('Lara','2009-06-03');
 
 SELECT * FROM sibling;
+
+# Age calculations
+
+SELECT name,DATE_FORMAT(birth,'%Y-%m-%d') as dob
+    , DATE_FORMAT(NOW(),'%Y-%m-%d') as today
+    , TIMESTAMPDIFF( YEAR, birth, NOW() ) as age_years
+    , TIMESTAMPDIFF( MONTH, birth, NOW() ) % 12 as age_months
+    , FLOOR( TIMESTAMPDIFF( DAY, birth, now() ) % 30.4375 ) as age_days
+ FROM sibling; 
+
+SELECT name, birth, '2009-06-09' AS 'Lara\'s birth',
+    TIMESTAMPDIFF( YEAR, birth,'2009-06-09' ) as age_years,
+    TIMESTAMPDIFF( MONTH, birth,'2009-06-09' ) % 12 as age_months,
+    FLOOR( TIMESTAMPDIFF( DAY, birth,'2009-06-09' ) % 30.4375 ) as age_days
+ FROM sibling WHERE name <> 'Lara';
